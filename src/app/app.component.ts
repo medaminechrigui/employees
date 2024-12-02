5fa2f7d42427fd80b1c5d8875e3fb98c7292a71a
medaminechrigui <chriguimohamedamine58@gmail.com>
2024-12-02 17:43:08 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { EmployeeService } from './services/employee.service';
+import { Employee } from './employee';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employees';
+
+
+
+title = 'app';
+employees = new Array<Employee>();
+constructor( empService:EmployeeService ) {
+empService.getEmployees().subscribe(response => this.employees = response )
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent
+],
+imports: [
+BrowserModule,
+HttpClientModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
